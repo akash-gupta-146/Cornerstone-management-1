0efbb9c9e4019a7c6918826bfe0fb8fddb01652f
NxtLife\Pankaj <kharetwal.pankaj111@gmail.com>
2017-06-22 18:28:27 +0530

    remove js and map

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var app_module_1 = require("./app/app.module");
-var core_1 = require("@angular/core");
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app/app.module');
+var core_1 = require('@angular/core');
 // Enable production mode unless running locally
 if (!/localhost/.test(document.location.host)) {
     core_1.enableProdMode();
